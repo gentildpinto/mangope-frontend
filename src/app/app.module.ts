573ed678c223326c66556942bd2c0a8672bc8ba9
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-11-08 13:36:31 +0100

    deleted land-tooltip directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './pages/map/map.component';
-import { LandTooltipDirective } from './directives/land-tooltip.directive';
 import { CountyHoverDirective } from './directives/county-hover.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    CountyHoverDirective,
-    // LandTooltipDirective
+    CountyHoverDirective
   ],
   imports: [
     BrowserModule
